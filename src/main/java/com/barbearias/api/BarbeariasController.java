af38285e041b2837f636070ae9abc9d2e9579c48
Melissa Ost <melissa-ost@hotmail.com>
2023-03-20 14:42:46 -0300

    crud completo

--- src/main/java/com/barbearias/api/BarbeariasController.java
+++ src/main/java/com/barbearias/api/BarbeariasController.java
@@ -1,11 +1,16 @@
 package com.barbearias.api;
 
+import com.barbearias.domain.Barbearia;
 import com.barbearias.domain.BarbeariaService;
+import com.barbearias.domain.dto.BarbeariaDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/barbearias")
@@ -15,6 +20,54 @@ public class BarbeariasController {
 
     @GetMapping()
     public ResponseEntity get(){
+        return ResponseEntity.ok(service.getBarbearias());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<List<BarbeariaDTO>> get(@PathVariable("id") Long id){
+        List<BarbeariaDTO> carro = service.getBarbeariaById(id);
+
+        return ResponseEntity.ok(carro);
+    }
+
+    @GetMapping("/bairro/{bairro}")
+    public ResponseEntity<List<BarbeariaDTO>> getByBairro(@PathVariable("bairro") String bairro){
+        List<BarbeariaDTO> barbearias = service.getByBairro(bairro);
+
+        return barbearias.isEmpty() ?
+                ResponseEntity.noContent().build() :
+                ResponseEntity.ok(barbearias);
+    }
+
+    @PostMapping
+    public ResponseEntity<BarbeariaDTO> post(@RequestBody Barbearia barbearia) {
+
+        BarbeariaDTO c = service.insert(barbearia);
+        return c != null ?
+                ResponseEntity.created(getUri(c.getId())).build() :
+                ResponseEntity.badRequest().build();
+    }
+
+    private URI getUri(Long id) {
+        return ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(id).toUri();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<BarbeariaDTO> put(@PathVariable("id") Long id, @RequestBody Barbearia barbearia) {
+
+        barbearia.setId(id);
+        BarbeariaDTO b = service.update(barbearia, id);
+
+        return b != null ?
+                ResponseEntity.ok(b) :
+                ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<List<BarbeariaDTO>> delete(@PathVariable("id") Long id) {
+
+        service.delete(id);
         return ResponseEntity.ok(service.getBarbearias());
     }
 }
--- src/main/java/com/barbearias/domain/BarbeariaService.java
+++ src/main/java/com/barbearias/domain/BarbeariaService.java
@@ -3,8 +3,10 @@ package com.barbearias.domain;
 import com.barbearias.domain.dto.BarbeariaDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -15,12 +17,43 @@ public class BarbeariaService {
 
     public List<BarbeariaDTO> getBarbearias() {
 
-        return rep.findAll().stream().map(c -> BarbeariaDTO.create(c)).collect(Collectors.toList());
-
-        // List<CarroDTO> list = new ArrayList<>();
-        // for(Carro c : carros) {
-        //		list.add(new CarroDTO(c));
-        // }
+        return rep.findAll().stream().map(b -> BarbeariaDTO.create(b)).collect(Collectors.toList());
 
     }
+
+    public List<BarbeariaDTO> getBarbeariaById(Long id) {
+        return rep.findById(id).stream().map(b -> BarbeariaDTO.create(b)).collect(Collectors.toList());
+
+    }
+
+    public List<BarbeariaDTO> getByBairro(String bairro) {
+        return rep.findByBairro(bairro).stream().map(b -> BarbeariaDTO.create(b)).collect(Collectors.toList());
+    }
+
+    public BarbeariaDTO insert(Barbearia barbearia) {
+        Assert.isNull(barbearia.getId(), "Não foi possível inserir registro");
+        return BarbeariaDTO.create(rep.save(barbearia));
+    }
+
+    public BarbeariaDTO update(Barbearia barbearia, Long id) {
+        Assert.notNull(id, "Não foi possível atualizar o registro.");
+
+        Optional<Barbearia> optional = rep.findById(id);
+        if(optional.isPresent()) {
+            Barbearia db = optional.get();
+            db.setNome(barbearia.getNome());
+            db.setRua(barbearia.getRua());
+            System.out.println("Bairro id: " + db.getId());
+
+            rep.save(db);
+            return BarbeariaDTO.create(db);
+        }else {
+            return null;
+        }
+    }
+
+    public void delete(Long id){
+        rep.deleteById(id);
+    }
+
 }
